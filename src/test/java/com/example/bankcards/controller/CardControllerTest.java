e5720962a7b864b952be7b8d9628e623128a3419
vsniadd2 <ivan.rodnenkov@mail.ru>
2025-11-19 02:09:38 +0300

    test 5

--- src/test/java/com/example/bankcards/controller/CardControllerTest.java
+++ src/test/java/com/example/bankcards/controller/CardControllerTest.java
@@ -1,8 +1,16 @@
 package com.example.bankcards.controller;
 
+import com.example.bankcards.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CardController.class)
-public class CardControllerTest {
+class CardControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockitoBean
+    private CardService cardService;
 }
